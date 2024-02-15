38ddd5223a25772befd8a5d60709a0db5e2f1664
pongsatorn <darkadura@gmail.com>
2024-02-15 11:28:08 +0700

    make it right bro

--- ชาบูหมาล่า/HolidayTravelVehicles/Car.java
+++ ชาบูหมาล่า/HolidayTravelVehicles/Car.java
@@ -1,18 +1,10 @@
-public class main {
-    public class car {
+public class car {
         private int vehicle_serial_number;
         private String name;
         private String model;
         private int year;
         private String manufacturer;
         private int basecost;
-        /* 
-        public car(String initialName) {
-            this.vehicle_serial_number;
-            this.name;
-
-        }
-        */
-    }
 
 }
+
