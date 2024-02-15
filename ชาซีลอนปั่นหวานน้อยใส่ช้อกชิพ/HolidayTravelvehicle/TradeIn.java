e1033ba6c2e3f78d88e9eba41107d8311946db18
John(Weerin) <baiyoklovedogs@gmail.com>
2024-02-15 11:49:26 +0700

    customer amazing wow

--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
@@ -1,17 +1,3 @@
 public class TradeIn {
-    private String serialNumber;
-    private String make;
-    private String model;
-    private int year;
-
-    public TradeIn(String serialNumber, String make, String model, int year) {
-        this.serialNumber = serialNumber;
-        this.make = make;
-        this.model = model;
-        this.year = year;
-    }
-
-    public String getSerialNumber(){
-        return serialNumber;
-    }
+    
 }
