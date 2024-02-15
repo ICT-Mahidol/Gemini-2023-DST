80ac18aa60577352d899c884090d34808c424cd7
Chanyanuch <chanyanuch.nua@student.mahidol.ac.th>
2024-02-15 11:27:30 +0700

    change code

--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
@@ -1,3 +1,13 @@
 public class TradeIn {
-    
+    private int serialNumber;
+    private String make;
+    private String model;
+    private int year;
+
+    public TradeIn(int serialNumber, String make, String model, int year) {
+        this.serialNumber = serialNumber;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
 }
