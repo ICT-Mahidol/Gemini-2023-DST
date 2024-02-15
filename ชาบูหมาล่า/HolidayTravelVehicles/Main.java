2cb59dfd187f21ac058ef01e90acd7ea54b79cd8
pongsatorn <darkadura@gmail.com>
2024-02-15 12:17:42 +0700

    fix attribute type

--- ชาบูหมาล่า/HolidayTravelVehicles/Main.java
+++ ชาบูหมาล่า/HolidayTravelVehicles/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void Main(String[] args) {
-        Car car = new Car(11502567, "Toyota", "Yaris", "2005", "Toyota Motor Corporation", 300000);
+        Car car = new Car(11502567, "Toyota", "Yaris", 2005, "Toyota Motor Corporation", 300000);
      
     System.out.println("Serial Number: ", + car.getVehicle_serial_number);
     System.out.println("Name: ", + car.getName);
