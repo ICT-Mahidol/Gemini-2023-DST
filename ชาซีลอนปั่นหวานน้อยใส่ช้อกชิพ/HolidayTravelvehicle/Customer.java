a2ca1687cfab306ff4af7f2a1fad410678edee00
John(Weerin) <baiyoklovedogs@gmail.com>
2024-02-15 11:48:58 +0700

    customer code

---
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/Customer.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Customer {
+    private String customerID;
+    public String name;
+    private String address;
+    private String phone_number;
+    // private ArrayList<SaleInvoice> saleInvoices;
+
+    // public ArrayList<SaleInvoice> getSaleInvoice(){
+    //     return this.saleInvoices;
+    // }
+    public Customer(String id,String name) {
+        this.customerID = id;
+        this.name = name;
+    }
+    
+    public Customer(String id,String name,String address) {
+        this.customerID = id;
+        this.name = name;
+        this.address = address;
+    }
+    
+    public Customer(String id,String name,String address,String phone_number) {
+        this.customerID = id;
+        this.name = name;
+        this.address = address;
+        this.phone_number = phone_number;
+    }
+    public String getName() {
+		return name;
+	}
+    public String getCustomerID() {
+		return customerID;
+	}
+    public String getAddress() {
+		return address;
+	}
+    public String getPhone(){
+        return phone_number;
+    }
+
+    
+}
