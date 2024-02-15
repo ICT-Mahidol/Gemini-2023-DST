f96f7fd313808727434bb58a492c9261cda202da
petch24096666 <petch24096666.sa@gmail.com>
2024-02-15 10:39:26 +0700

    test

---
+++ ฟอมเฟี้ยว/HolidayTravelVehicels/Customer.java
@@ -0,0 +1,6 @@
+public class Customer {
+    public String customerID;
+    public String name;
+    public int phoneNumber;
+}
+
---
+++ ฟอมเฟี้ยว/HolidayTravelVehicels/Options.java
@@ -0,0 +1,5 @@
+public class Options {
+    public int option_code;
+    public String descripition;
+    public int price;
+}
