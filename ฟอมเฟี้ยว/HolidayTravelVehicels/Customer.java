b575cb2b13c4f8e22461967c55335a29dd581a0c
petch24096666 <petch24096666.sa@gmail.com>
2024-02-15 11:13:45 +0700

    test

--- ฟอมเฟี้ยว/HolidayTravelVehicels/Customer.java
+++ ฟอมเฟี้ยว/HolidayTravelVehicels/Customer.java
@@ -1,6 +1,25 @@
 public class Customer {
-    public String customerID;
+    private int customerID;
     public String name;
+    public String address;
     public int phoneNumber;
-}
 
+    public Customer(int newCustomerID, String newName, String newAddress, int newPhonenumber) {
+        this.customerID = newCustomerID;
+        this.name = newName;
+        this.address = newAddress;
+        this.phoneNumber = newPhonenumber;
+    }
+    public int getCustomerID() {
+        return this.customerID;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getAddress() {
+        return this.address;
+    }
+    public int getPhonenumber(){
+        return this.phoneNumber;
+    }
+}
--- ฟอมเฟี้ยว/HolidayTravelVehicels/Options.java
+++ ฟอมเฟี้ยว/HolidayTravelVehicels/Options.java
@@ -2,4 +2,8 @@ public class Options {
     public int option_code;
     public String descripition;
     public int price;
+
+    public Options(int newOptions, String newName, String newAddress, int newPhonenumber) {
+        
+    }
 }
