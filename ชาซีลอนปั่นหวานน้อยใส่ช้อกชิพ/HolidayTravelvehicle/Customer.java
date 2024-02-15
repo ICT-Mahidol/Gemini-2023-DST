bbad2303df716f459c0c3d26540774f5b883cb60
John(Weerin) <baiyoklovedogs@gmail.com>
2024-02-15 12:22:19 +0700

    change customer again hi

--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/Customer.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/Customer.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-
-
 public class Customer {
     private String customerID;
     public String name;
@@ -11,16 +8,16 @@ public class Customer {
     // public ArrayList<SaleInvoice> getSaleInvoice(){
     //     return this.saleInvoices;
     // }
-    public Customer(String id,String name) {
-        this.customerID = id;
-        this.name = name;
-    }
+    // public Customer(String id,String name) {
+    //     this.customerID = id;
+    //     this.name = name;
+    // }
     
-    public Customer(String id,String name,String address) {
-        this.customerID = id;
-        this.name = name;
-        this.address = address;
-    }
+    // public Customer(String id,String name,String address) {
+    //     this.customerID = id;
+    //     this.name = name;
+    //     this.address = address;
+    // }
     
     public Customer(String id,String name,String address,String phone_number) {
         this.customerID = id;
@@ -41,5 +38,20 @@ public class Customer {
         return phone_number;
     }
 
+    public void setName(String name) {
+		this.name = name;
+	}
+    public void setCustomerID(String id ) {
+		this.customerID = id;
+	}
+    public void setAddress(String address) {
+		this.address = address;
+	}
+    public void setPhone(String phone){
+        this.phone_number = phone;
+    }
 
+    public String toString(){
+        return "ID: " + customerID + "Name: " + name + "Address: " + address + "Phone: " + phone_number;
+    }
 }
