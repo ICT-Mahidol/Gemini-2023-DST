67f509a73cf8069ad12b4ccc5b08bfb6df5f517d
Thanakrit031 <thanakrit.sir@student.mahidol.edu>
2024-02-15 10:51:10 +0700

    Update Employee.java

--- 2023-ITDS361-1/HolidayTravelVehicles/Employee.java
+++ 2023-ITDS361-1/HolidayTravelVehicles/Employee.java
@@ -1,3 +1,28 @@
 public class Employee {
-    
+    private String employeeID;
+    private String employeeName;
+    private String phoneNumber;
+
+    public Employee(){
+        
+    }
+    public Employee(String employeeID,String employeeName, String phoneNumber) {
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.phoneNumber = phoneNumber;
+    }
+    public String getemployeeID() {
+        return employeeID;
+    }
+    public String getemployeeName() {
+        return employeeName;
+    }
+    public String getphoneNumber() {
+        return phoneNumber;
+    }
+    public void setemployeeProfile(String employeeID,String employeeName, String phoneNumber){
+        this.employeeID = employeeID;
+        this.employeeName = employeeName;
+        this.phoneNumber = phoneNumber;
+    }
 }
