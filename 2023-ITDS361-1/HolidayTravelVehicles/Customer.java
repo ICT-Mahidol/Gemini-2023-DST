7e2277868ae6a4668b83d4289e3ba81c62af1af5
Mind Ravisara <ravisara.wil@student.mahidol.edu>
2024-02-15 10:40:24 +0700

    getter setter Customer.java

--- 2023-ITDS361-1/HolidayTravelVehicles/Customer.java
+++ 2023-ITDS361-1/HolidayTravelVehicles/Customer.java
@@ -1,17 +1,46 @@
 public class Customer {
     private int customerID;
+    private String custormerName, address;
+    private int phoneNumber;
 
     public Customer(){}
 
     public Customer(int customerID, String custormerName, String address, int phoneNumber){
         this.customerID = customerID;
+        this.custormerName = custormerName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
     }
 
     public int getCustomerID(){
         return customerID;
     }
 
+    public String getCustomerName(){
+        return custormerName;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public int getPhoneNumber(){
+        return phoneNumber;
+    }
+
     public void setCustomerID(int customerID){
         this.customerID = customerID;
+    }
+
+    public void setCustomerName(){
+        this.custormerName = custormerName;
+    }
+
+    public void setAddress(){
+        this.address = address;
+    }
+
+    public void setPhoneNumber(){
+        this.phoneNumber = phoneNumber;
     }
 }
