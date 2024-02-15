24ed0a27c65b1e2aad27caeb38b90c1b05ed6e16
Mind Ravisara <ravisara.wil@student.mahidol.edu>
2024-02-15 22:17:20 +0700

    format

--- 2023-ITDS361-1/HolidayTravelVehicles/Main.java
+++ 2023-ITDS361-1/HolidayTravelVehicles/Main.java
@@ -2,26 +2,23 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]){
         
-        System.out.println("============================Customer Information============================");
+        System.out.println("\nCustomer Information------------------");
         Customer aCustomer = new Customer("001","Mrs. Robbinson Hobter",
         "999 Phutthamonthon Sai 4 Rd, Salaya, Phutthamonthon District, Nakhon Pathom 73170", "0899999999");
         aCustomer.addTradeInVehicle(
             new TradeInVehicle("601SEW","Big","AX01",2000));
         System.out.println(aCustomer.toString());
         aCustomer.showTradeInlist();
-        System.out.println();
 
-        System.out.println("============================Employee Information============================");
+        System.out.println("\nEmployee Information------------------");
         Employee  anEmployee = new Employee("01","Miss. Alice Berman","0245789136");
         System.out.println(anEmployee.toString());
-        System.out.println();
 
-        System.out.println("============================Vehicle Information============================");
+        System.out.println("\nVehicle Information------------------");
         Vehicle vehicles  = new Vehicle("A01","Austin","X1",2000,"toyota",1000000);
         System.out.println(vehicles.toString());
-        System.out.println();
 
-        System.out.println("============================Installed Options============================");
+        System.out.println("\nInstalled Options------------------");
         DealerInstalledOptions dealerInstalledOptions = new DealerInstalledOptions("Add door", "10001" , 40000);
         System.out.println(dealerInstalledOptions.toString());
 
