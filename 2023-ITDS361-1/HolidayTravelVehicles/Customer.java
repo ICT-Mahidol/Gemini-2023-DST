515eb64166585eb55593c3ff2aa26c28f2be6cbe
Mind Ravisara <ravisara.wil@student.mahidol.edu>
2024-02-15 10:28:56 +0700

    Customer.java

--- 2023-ITDS361-1/HolidayTravelVehicles/Customer.java
+++ 2023-ITDS361-1/HolidayTravelVehicles/Customer.java
@@ -1,3 +1,17 @@
 public class Customer {
-    
+    private int customerID;
+
+    public Customer(){}
+
+    public Customer(int customerID, String custormerName, String address, int phoneNumber){
+        this.customerID = customerID;
+    }
+
+    public int getCustomerID(){
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID){
+        this.customerID = customerID;
+    }
 }
