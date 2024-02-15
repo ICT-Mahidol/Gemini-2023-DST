835d15a62083f64c0729349659d4b10f9e2e9185
pongsatorn <darkadura@gmail.com>
2024-02-15 11:11:31 +0700

    create class

---
+++ ชาบูหมาล่า/HolidayTravelVehicles/Car.java
@@ -0,0 +1,26 @@
+public class main {
+    printCarDetail
+    public class car {
+        private int vehicle_serial_number;
+        private String name;
+        private String model;
+        private int year;
+        private String manufacturer;
+        private int basecost;
+        /* 
+        public car(String initialName) {
+            this.vehicle_serial_number;
+            this.name;
+
+        }
+        */
+        public void printCarDetail(){
+            System.out.println(this);
+        }
+
+        public static void main(String[] args) {
+            myMethod();
+          }
+    }
+
+}
