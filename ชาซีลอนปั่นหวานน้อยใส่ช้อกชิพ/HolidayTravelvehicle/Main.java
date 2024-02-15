c6c2e3ebbc9d9cc10feb85ca325f4e8210e82532
tunyarat <tunyarat.nit@student.mahidol.edu>
2024-02-15 11:25:53 +0700

    edit SaleInvoice

--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/Main.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/Main.java
@@ -3,12 +3,6 @@ public class Main {
     public static void main(String[] args) {
         SaleInvoice invoice1 = new SaleInvoice("5555");
         invoice1.printInfo();
-
-
-
-
-
-        
     }
     
 }
