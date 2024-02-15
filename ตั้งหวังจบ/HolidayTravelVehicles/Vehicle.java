63d26382b6a53c6226f231745bd5167cfa740c82
Sujasinee Kodcharin <sujasinee.kod@student.mahidol.edu>
2024-02-15 11:56:37 +0700

    Vehicle Java File

--- ตั้งหวังจบ/HolidayTravelVehicles/Vehicle.java
+++ ตั้งหวังจบ/HolidayTravelVehicles/Vehicle.java
@@ -55,27 +55,3 @@ public class Vehicle {
    }
 }
 
-public class Main {
-   public static void main(String[] args) {
-
-       // สร้างอ็อบเจกต์ของคลาส Vehicle
-       Vehicle car = new Vehicle(123456, "Toyota Camry", 2022, "Camry", "Toyota", 25000);
-
-       // แสดงข้อมูลของรถ
-       System.out.println("Serial Number: " + car.getSerialNumber());
-       System.out.println("Model: " + car.getModel());
-       System.out.println("Year: " + car.getYear());
-       System.out.println("Name: " + car.getName());
-       System.out.println("Manufacturer: " + car.getManufacturer());
-       System.out.println("Base Cost: $" + car.getBaseCost());
-
-       // อัพเดทข้อมูลรถ
-       car.setModel("Toyota Corolla");
-       car.setBaseCost(20000);
-       
-       // แสดงข้อมูลหลังอัพเดท
-       System.out.println("\nAfter Update:");
-       System.out.println("Model: " + car.getModel());
-       System.out.println("Base Cost: $" + car.getBaseCost());
-   }
-}
