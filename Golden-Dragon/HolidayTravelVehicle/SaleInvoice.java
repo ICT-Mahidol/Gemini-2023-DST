0ecb78996de3802a9a228b5ae668c64de32f6842
Talabporn Wutthicharoenkit <talabporn.wut@student.mahidol.edu>
2024-02-15 11:16:13 +0700

    pleng commit

--- Golden-Dragon/HolidayTravelVehicle/SaleInvoice.java
+++ Golden-Dragon/HolidayTravelVehicle/SaleInvoice.java
@@ -1,5 +1,17 @@
 package HolidayTravelVehicle;
 
 public class SaleInvoice {
-    
+    private String customerInfo;
+    private String tradeInVehicle;
+    private int tradeInAllowance;
+    private int vehicleFee;
+    private int finalCost;
+
+    public SaleInvoice(String customerInfo, String tradeInVehicle, int tradeInAllowance, int vehicleFee, int finalCost) {
+        this.customerInfo = customerInfo;
+        this.tradeInVehicle = tradeInVehicle;
+        this.tradeInAllowance = tradeInAllowance;
+        this.vehicleFee = vehicleFee;
+        this.finalCost = finalCost;
+    }
 }
