db75a76d94b92d14a4246e3e137e3105233a2ea5
Poramaporn Plaipool <poramaporn.pla@student.mahidol.edu>
2024-02-15 12:03:14 +0700

    customer

---
+++ ตั้งหวังจบ/HolidayTravelVehicles/Customer.java
@@ -0,0 +1,10 @@
+public class Person{
+    Private String firstname;
+    Private String lastname;
+}
+public class Customer extends Person{
+    Private int customerID;
+    Private String name;
+    Private String address;
+    Private String phonenumber;
+}
