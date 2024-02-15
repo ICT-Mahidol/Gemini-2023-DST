83efa66a7c5edeb9deec95edceed6d886eee73a9
Chanyanuch <chanyanuch.nua@student.mahidol.ac.th>
2024-02-15 11:44:50 +0700

    edit code

--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/DealerInstalled.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/DealerInstalled.java
@@ -9,4 +9,22 @@ public class DealerInstalled {
         this.price = price;
     }
 
+    public int getOptionCode(){
+        return optionCode;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getPrice(){
+        return price;
+    }
+
+    public void printInfo() {
+        System.out.println("Option Code: " + getOptionCode());
+        System.out.println("Description: " + getDescription());
+        System.out.println("Price: " + getPrice());
+    }
+
 }
--- ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
+++ ชาซีลอนปั่นหวานน้อยใส่ช้อกชิพ/HolidayTravelvehicle/TradeIn.java
@@ -14,4 +14,24 @@ public class TradeIn {
     public String getSerialNumber(){
         return serialNumber;
     }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void printInfo() {
+        System.out.println("Serial Number: " + getSerialNumber());
+        System.out.println("Make: " + getMake());
+        System.out.println("Model: " + getModel());
+        System.out.println("Year: " + getYear());
+    }
+
 }
