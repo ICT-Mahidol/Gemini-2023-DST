3ead09cbe52ef7483e55c2998133ca668d5480d7
Sujasinee Kodcharin <sujasinee.kod@student.mahidol.edu>
2024-02-15 11:29:13 +0700

    Vehicle.java

---
+++ ตั้งหวังจบ/HolidayTravelVehicles/Vehicle.java
@@ -0,0 +1,8 @@
+public class Vehicle {
+    private int SerialNumber;
+    private String Model;
+    private int Year;
+    public String Name;
+    public String Manufacturer;
+    public int BaseCost;
+}
