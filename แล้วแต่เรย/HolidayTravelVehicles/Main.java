009a7fb32f73aa421dd7580db3d1c587ee1f31ad
ssil65 <realkan54xx@gmail.com>
2024-02-15 11:35:57 +0700

    add main class

---
+++ แล้วแต่เรย/HolidayTravelVehicles/Main.java
@@ -0,0 +1,9 @@
+/* import .... */
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vehical FirstCar = new Vehical("111111111", "LTL", "Y3", 2023, "ICT", 1000000);
+        system.out.println(FirstCar.toString());
+    }
+}
