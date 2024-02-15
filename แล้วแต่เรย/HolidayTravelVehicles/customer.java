65b9789351f6a9ccc5255bd90d4b4ffa3c354ea9
Ravipitch Laosaengchaiyawat <ravipitch.lao@student.mahidol.edu>
2024-02-15 12:00:27 +0700

    Update Customer class

---
+++ แล้วแต่เรย/HolidayTravelVehicles/customer.java
@@ -0,0 +1,32 @@
+package แล้วแต่เรย.HolidayTravelVehicles;
+
+public class customer {
+    public int CustomerID;
+    public String Name;
+    public String Address;
+    public int PhoneNumber;
+
+    // constructor
+    public customer() {
+        this.CustomerID = CustomerID;
+        this.Name = Name;
+        this.Address = Address;
+        this.PhoneNumber = PhoneNumber;
+    }
+
+    public int GetCusomer() {
+        return this.CustomerID;
+    }
+
+    public String GetName() {
+        return this.Name;
+    }
+
+    public String GetAddress() {
+        return this.Address;
+    }
+
+    public int GetPhoneNumber() {
+        return this.PhoneNumber;
+    }
+}
