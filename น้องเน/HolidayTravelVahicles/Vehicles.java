276a27d93815490543b9aaf5d0bbe8cd34eb2eae
ThanaSavey <Thanaddanai@hotmail.com>
2024-02-15 11:45:13 +0700

    test

---
+++ น้องเน/HolidayTravelVahicles/Vehicles.java
@@ -0,0 +1,20 @@
+//Vehicles
+public class Vehicle {
+    private String vehicleserialnumber;
+    private String name;
+    private String model;
+    private String manufacturer;
+    private int year;
+    private int basecost;
+}
+
+//Constructor
+public salesinvoice(String full, String customerinformation, String tradeinvehicle, String tradeinallowance, String dealerinstalledoptions) {
+    this.full = full;
+    this.customerinformation = CustomerInformation;
+    this.tradeinvehivle = TradeinVehicle;
+    this.tradeinallowance = TradeinAllowance;
+    this.dealerinstalledoptions = DealerInstallOption;
+}
+
+//Associations
