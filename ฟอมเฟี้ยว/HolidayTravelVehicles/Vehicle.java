882205358582ee54d90c2a97b1b146e58bed0c14
knotekkasit <114134393+knotekkasit@users.noreply.github.com>
2024-02-20 14:20:08 +0700

    Create Vehicle.java

---
+++ ฟอมเฟี้ยว/HolidayTravelVehicles/Vehicle.java
@@ -0,0 +1,43 @@
+public class Vehicle {
+    private String serialnumber;
+    public String name;
+    public String model;
+    public int year;
+    public String manufacturer;
+    public int basecost;
+    private Saleinvoice saleinvoice;
+
+
+    public Vehicle(String serialnumber, String name, String model, int year, String manufacturer, int basecost) {
+        this.serialnumber = serialnumber;
+        this.name = name;
+        this.model = model;
+        this.year = year;
+        this.manufacturer = manufacturer;
+        this.basecost = basecost;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getSerialNumber() {
+        return this.serialnumber;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+    
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getManufacturer() {
+        return this.manufacturer;
+    }
+
+    public int getBaseCost() {
+        return this.basecost;
+    }
+}
