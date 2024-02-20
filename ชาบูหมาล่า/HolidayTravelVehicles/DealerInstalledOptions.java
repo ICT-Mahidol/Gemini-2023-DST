de1d08db65aced2dec35ab863f2db03b9df99fd0
Kullatida <kullatida.jag@student.mahidol.edu>
2024-02-20 18:35:16 +0700

    Dealer Update

--- ชาบูหมาล่า/HolidayTravelVehicles/DealerInstalledOptions.java
+++ ชาบูหมาล่า/HolidayTravelVehicles/DealerInstalledOptions.java
@@ -1,5 +1,16 @@
 public class DealerInstalledOptions {
     public int optioncode;
     public String description;
-    public int price;   
+    public int price;  
+    
+    
+    public void printDealerInstalledOptions(int optioncode, String description, int price) {
+        this.optioncode = optioncode;
+        this.description = description;
+        this.price = price;
+    
+        System.out.println("Option Code: " + optioncode);
+        System.out.println("Description: " + description);
+        System.out.println("Price: " + price);
+    }
 };
