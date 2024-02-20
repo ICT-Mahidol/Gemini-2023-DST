a5395db23eacf421b2cbabeaf16c8ff223011a70
Kullatida <kullatida.jag@student.mahidol.edu>
2024-02-20 18:33:04 +0700

    SalesPerson update

--- ชาบูหมาล่า/HolidayTravelVehicles/SalesPerson.java
+++ ชาบูหมาล่า/HolidayTravelVehicles/SalesPerson.java
@@ -1,4 +1,33 @@
 public class SalesPerson {
-    public int employeeID;
-    public String name; 
+    private int employeeID;
+    private String firstName; 
+    private String lastName; 
+
+    //Constructor
+    public SalesPerson (int employeeID, String firstName, String lastName) {
+        this.employeeID = employeeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Get method
+    public int getEmployeeID() {
+        return employeeID;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    //Set method
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
