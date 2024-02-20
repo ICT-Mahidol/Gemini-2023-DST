b75e110d8b3b0fc7b3288cfadd6e66292ca45941
jrpsun <114118636+jrpsun@users.noreply.github.com>
2024-02-20 18:38:41 +0700

    Update Main.java

--- แล้วแต่เรย/HolidayTravelVehicles/Main.java
+++ แล้วแต่เรย/HolidayTravelVehicles/Main.java
@@ -1,9 +1,7 @@
-/* import .... */
-
 public class Main {
 
     public static void main(String[] args) {
-        Vehical FirstCar = new Vehical("111111111", "LTL", "Y3", 2023, "ICT", 1000000);
-        system.out.println(FirstCar.toString());
+        Vehicle FirstCar = new Vehicle(0, "LTL", "Y3", 2023, "ICT", 1000000);
+        System.out.println(FirstCar.toString());
     }
 }
