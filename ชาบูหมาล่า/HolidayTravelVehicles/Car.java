fd7abf42d4b297862c1ddfe4043f1dd214395c15
Besom0rph <94701330+Pongsatorn6487048@users.noreply.github.com>
2024-02-22 12:19:24 +0700

    Update Car.java

--- ชาบูหมาล่า/HolidayTravelVehicles/Car.java
+++ ชาบูหมาล่า/HolidayTravelVehicles/Car.java
@@ -6,6 +6,7 @@ public class Car {
         private String manufacturer;
         private int basecost;
 
+        #constructor
         public Car (int vehicle_serial_number, String name, String model, int year, String manufacturer, int basecost) {
             this.vehicle_serial_number = vehicle_serial_number;
             this.name = name;
